731ae81e600f5f6bd1e786efc93fa155bd967e60
wuyxz <wuyxz@wuyxzs-MacBook-Pro.local>
2021-06-25 22:04:56 +0700

    con cai nit

--- routers/Product.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {ProductController} from '../controllers/Product.js';
-
-const router = express.Router();
-
-router.get('/', ProductController);
-
-export default router;
--- routers/menu.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { CategoryController } from '../controllers/menu.js';
-
-const router = express.Router();
-
-router.get('/',CategoryController);
-
-
-export default router;
