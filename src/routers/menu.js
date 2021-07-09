3531cab66a2fe92c58b25b0a35680329ba1e923e
TranSonDinh <sondinh2703@gmail.com>
2021-07-09 10:29:49 +0700

    reup2

--- src/routers/menu.js
+++ src/routers/menu.js
@@ -4,7 +4,5 @@ import { getProductsByCategory } from "../controllers/ProductsController.js";
 const router = express.Router();
 
 router.get("/", getCategories);
-router.get("/:index", getCategories);
-router.get("/:index/:page", getCategories);
 
 export default router;
