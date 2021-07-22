5f219950b985e8c4c8e21aa97b5b75c88cb18a7d
TranSonDinh <sondinh2703@gmail.com>
2021-07-23 00:08:57 +0700

    done profile user

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -1,7 +1,6 @@
-import passport from "passport";
+import {check, validationResult} from 'express-validator';
 import bcrypt from 'bcryptjs';
 import { Customers } from "../models/Customer.js";
-import { Location } from "../models/Location.js";
 
 export const index = async (req, res) => {
   try {
@@ -26,30 +25,43 @@ export const index = async (req, res) => {
 
 export const updateCustomers = async (req, res) => {
   const profile = req.body;
+  let sess = req.session;
   try{
-    let passwordChange;
-    let User = {
-      province: profile.address_provinces,
-      district: profile.address_district,
-      detail: profile.address,
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      res.render("frontend/profile", {
+        singinup: true,
+        showHeader: true,
+        showHeaderContent: false,
+        showCart: true,
+        layout: "home-layout",
+        errorPass: true,
+        user: {user: sess.user, isExist: sess.user ? true : false},
+      });
+    }else {
+      let passwordChange;
+      let User = {
+        province: profile.address_provinces,
+        district: profile.address_district,
+        detail: profile.address,
+      }
+      if(profile.pass !== "" && profile.re_pass !== ""){
+        let salt = bcrypt.genSaltSync(10);
+        passwordChange = bcrypt.hashSync(profile.pass, salt);
+        User = {...User, password: passwordChange};
+      }
+      await Customers.findOneAndUpdate({email: profile.email}, {name: profile.name, phone: profile.phone, ...User});
+      let proUpdate = {name: profile.name, email: profile.email, phone: profile.phone ,...User};
+      sess.user = proUpdate;
+      res.render("frontend/profile", {
+        singinup: true,
+        showHeader: true,
+        showHeaderContent: false,
+        showCart: true,
+        layout: "home-layout",
+        user: {user: proUpdate, isExist: proUpdate ? true : false},
+      });
     }
-    if(profile.pass !== ""){
-      let salt = bcrypt.genSaltSync(10);
-      passwordChange = bcrypt.hashSync(profile.pass, salt);
-      User = {...User, password: passwordChange};
-    }
-    await Customers.findOneAndUpdate({email: profile.email}, {name: profile.name, phone: profile.phone, ...User});
-    let sess = req.session;
-    let proUpdate = {name: profile.name, email: profile.email, phone: profile.phone ,...User};
-    sess.user = proUpdate;
-    res.render("frontend/profile", {
-      singinup: true,
-      showHeader: true,
-      showHeaderContent: false,
-      showCart: true,
-      layout: "home-layout",
-      user: {user: proUpdate, isExist: proUpdate ? true : false},
-    });
   } catch(err){
     res.status(500).json("error", err);
   }
--- src/routers/profileUser.js
+++ src/routers/profileUser.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import {check} from 'express-validator';
 import {getProfile} from "../controllers/HomeController.js";
 import {updateCustomers} from '../controllers/CustomersController.js';
 const Router = express.Router();
 
 Router.get("/", getProfile);
-Router.post("/", updateCustomers);
+Router.post("/", [
+    check("pass", "Invalid password")
+        .custom((value,{req, loc, path}) => {
+            if (value !== req.body.re_pass || value.length < 4 && value !== "") {
+                throw new Error("Passwords don't match");
+            } else {
+                return true;
+            }
+        }),
+  ],
+  updateCustomers);
 
 export default Router;
